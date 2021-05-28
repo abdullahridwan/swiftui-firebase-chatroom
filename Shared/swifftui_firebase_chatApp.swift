06f56e01303140e7eb795e1e9075de72c4494eb9
Abdullah Ridwan <abdullahridwan73@gmail.com>
2021-05-28 13:04:39 -0400

    Initial Commit

---
+++ Shared/swifftui_firebase_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifftui_firebase_chatApp.swift
+//  Shared
+//
+//  Created by Abdullah Ridwan on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct swifftui_firebase_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
